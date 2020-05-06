4cfd37207cead3578d5c98a18dd2584c993fdafd
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2020-05-06 15:18:44 +0300

    Add files via upload

---
+++ lib/ui/QrCode/scan.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class ScanScreen extends StatefulWidget {
+  @override
+  _ScanState createState() => new _ScanState();
+}
+
+class _ScanState extends State<ScanScreen> {
+  final Firestore _firestore = Firestore.instance;
+  String barcode = "";
+  String result;
+
+  String _scanBarcode = "";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        barcode = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          barcode = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          barcode = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        barcode = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        barcode = "Unknown Error $ex";
+      });
+    }
+
+    setState(() {
+      result = "Yoklama alınıyor...";
+    });
+
+    final DocumentReference eventRef =
+    _firestore.document("Etkinlikler/$barcode");
+
+    _firestore.runTransaction((Transaction transaction) async {
+      DocumentSnapshot eventData = await eventRef.get();
+      List katilimcilar = eventData.data["Katılımcılar"];
+      katilimcilar.add("Hakkıcan Bülüç");
+      await transaction.update(eventRef, {"Katılımcılar": katilimcilar}).then(
+              (onValue) {
+                setState(() {
+                  result = "Yoklama alındı";
+                });
+              }).catchError((onError) {
+                setState(() {
+                  result = "Yoklama alınırken sorun oluştu";
+                });
+      });
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text('Yoklama'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    splashColor: Colors.blueGrey,
+                    onPressed: _scanQR,
+                    child: const Text('START CAMERA SCAN')
+                ),
+              )
+              ,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(result, textAlign: TextAlign.center,),
+              )
+              ,
+            ],
+          ),
+        ));
+  }
+}
+
